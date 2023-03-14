98ec82687cc38df6374f5897376e9ce003adb361
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2023-03-14 11:51:20 -0300

    feat: refactor Funcionario class

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,4 +1,5 @@
-﻿using bytebank.Modelos.Conta;
+﻿using bytebank.Modelos.ADM.Funcionarios;
+using bytebank.Modelos.Conta;
 using bytebank_ATENDIMENTO.bytebank.Exceptions;
 using bytebank_ATENDIMENTO.bytebank.Util;
 using bytebank_ATENDIMENTO.byteBank_Atendimento;
@@ -34,3 +35,20 @@ ArrayContasCorrentes();
 #endregion
 
 new ByteBankAtendimento().AtendimentoCliente();
+
+public class Estagiario : Funcionario
+{
+    public Estagiario(double salario, string cpf) : base(salario, cpf)
+    {
+    }
+
+    public override void AumentarSalario()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override double getBonificacao()
+    {
+        throw new NotImplementedException();
+    }
+}
